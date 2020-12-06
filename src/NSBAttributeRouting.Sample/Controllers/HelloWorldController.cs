625fc8bb0db2cca512e862adc8c0d8689a6f8de7
Stephen Bertrand <stephenb@itworks.com>
2020-12-06 11:34:49 -0500

    empty project

---
+++ src/NSBAttributeRouting.Sample/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSBAttributeRouting.Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        public HelloWorldController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
